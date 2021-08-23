c24ed2383845855108922f3b8ad4b14a6efac6a8
Pavel Korchagin <mortalis94@gmail.com>
2021-08-23 22:08:06 +0300

    Implemented test for search server (4 week)

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -11,6 +11,7 @@ import (
 	"net/url"
 	"os"
 	"reflect"
+	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -96,10 +97,9 @@ var (
 )
 
 const (
-	IdField    = "Id"
-	AgeField   = "Age"
-	NameField  = "Name"
-	AboutField = "About"
+	IdField   = "Id"
+	AgeField  = "Age"
+	NameField = "Name"
 )
 
 type QueryParams struct {
@@ -160,7 +160,7 @@ func (q QueryParamsExtractor) OrderField() (string, error) {
 	switch orderField {
 	case "":
 		return NameField, nil
-	case IdField, NameField, AboutField:
+	case IdField, NameField, AgeField:
 		return orderField, nil
 	default:
 		return "", fmt.Errorf("unknown order field %q", orderField)
@@ -222,8 +222,10 @@ type ServerUser struct {
 	Gender    string `json:"gender" xml:"gender"`
 }
 
+const AccessToken = "aGVsbG86d29ybGQ="
+
 func SearchServer(w http.ResponseWriter, req *http.Request) {
-	if req.Header.Get("AccessToken") != "aGVsbG86d29ybGQ=" {
+	if req.Header.Get("AccessToken") != AccessToken {
 		sendErrorJSONResponse(w, errUnauthorized)
 		return
 	}
@@ -309,6 +311,10 @@ func sendInvalidJSONResponse(w http.ResponseWriter, statusCode int) {
 func getUsers(reader io.Reader, qParams QueryParams) ([]*ServerUser, error) {
 	users := make([]*ServerUser, 0)
 	decoder := xml.NewDecoder(reader)
+	pattern, err := regexp.Compile(`\s+`)
+	if err != nil {
+		return nil, err
+	}
 
 	query := strings.ToLower(qParams.Query)
 	for {
@@ -333,6 +339,12 @@ func getUsers(reader io.Reader, qParams QueryParams) ([]*ServerUser, error) {
 					continue
 				}
 
+				about := pattern.ReplaceAllString(user.About, " ")
+				if about[len(about)-1] == ' ' {
+					about = about[:len(about)-1]
+				}
+
+				user.About = about
 				users = append(users, user)
 			}
 		}
@@ -390,13 +402,13 @@ func getSortFunc(users []*ServerUser, orderField string, orderBy int) SortFunc {
 	return nil
 }
 
-type TestCase struct {
+type UserTestCase struct {
 	QueryParams SearchRequest
 	IsError     bool
 }
 
 func TestFindUsers(t *testing.T) {
-	testCases := []TestCase{
+	testCases := []UserTestCase{
 		{
 			QueryParams: SearchRequest{Query: "lorem a"},
 		},
@@ -443,7 +455,7 @@ func TestFindUsers(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(SearchServer))
 	client := &SearchClient{
 		URL:         server.URL,
-		AccessToken: "aGVsbG86d29ybGQ=",
+		AccessToken: AccessToken,
 	}
 
 	for testNum, testCase := range testCases {
@@ -468,8 +480,301 @@ func TestFindUserUnauthorized(t *testing.T) {
 }
 
 func TestFindUserBadURL(t *testing.T) {
-	client := &SearchClient{URL: "http://localhost:100", AccessToken: "aGVsbG86d29ybGQ="}
+	client := &SearchClient{URL: "http://localhost:100", AccessToken: AccessToken}
 	if _, err := client.FindUsers(SearchRequest{Query: "tim"}); err == nil {
 		t.Errorf("got nil, expected error")
+	}
+}
+
+type ServerTestCase struct {
+	StatusCode  int
+	QueryParams url.Values
+	Result      []ServerUser
+}
+
+var testUsers = []ServerUser{
+	{
+		Id:     0,
+		Name:   "Boyd Wolf",
+		Age:    22,
+		About:  "Nulla cillum enim voluptate consequat laborum esse excepteur occaecat commodo nostrud excepteur ut cupidatat. Occaecat minim incididunt ut proident ad sint nostrud ad laborum sint pariatur. Ut nulla commodo dolore officia. Consequat anim eiusmod amet commodo eiusmod deserunt culpa. Ea sit dolore nostrud cillum proident nisi mollit est Lorem pariatur. Lorem aute officia deserunt dolor nisi aliqua consequat nulla nostrud ipsum irure id deserunt dolore. Minim reprehenderit nulla exercitation labore ipsum.",
+		Gender: "male",
+	},
+	{
+		Id:     5,
+		Name:   "Beulah Stark",
+		Age:    30,
+		About:  "Enim cillum eu cillum velit labore. In sint esse nulla occaecat voluptate pariatur aliqua aliqua non officia nulla aliqua. Fugiat nostrud irure officia minim cupidatat laborum ad incididunt dolore. Fugiat nostrud eiusmod ex ea nulla commodo. Reprehenderit sint qui anim non ad id adipisicing qui officia Lorem.",
+		Gender: "female",
+	},
+	{
+		Id:     6,
+		Name:   "Jennings Mays",
+		Age:    39,
+		About:  "Veniam consectetur non non aliquip exercitation quis qui. Aliquip duis ut ad commodo consequat ipsum cupidatat id anim voluptate deserunt enim laboris. Sunt nostrud voluptate do est tempor esse anim pariatur. Ea do amet Lorem in mollit ipsum irure Lorem exercitation. Exercitation deserunt adipisicing nulla aute ex amet sint tempor incididunt magna. Quis et consectetur dolor nulla reprehenderit culpa laboris voluptate ut mollit. Qui ipsum nisi ullamco sit exercitation nisi magna fugiat anim consectetur officia.",
+		Gender: "male",
+	},
+	{
+		Id:     7,
+		Name:   "Leann Travis",
+		Age:    34,
+		About:  "Lorem magna dolore et velit ut officia. Cupidatat deserunt elit mollit amet nulla voluptate sit. Quis aute aliquip officia deserunt sint sint nisi. Laboris sit et ea dolore consequat laboris non. Consequat do enim excepteur qui mollit consectetur eiusmod laborum ut duis mollit dolor est. Excepteur amet duis enim laborum aliqua nulla ea minim.",
+		Gender: "female",
+	},
+	{
+		Id:     8,
+		Name:   "Glenn Jordan",
+		Age:    29,
+		About:  "Duis reprehenderit sit velit exercitation non aliqua magna quis ad excepteur anim. Eu cillum cupidatat sit magna cillum irure occaecat sunt officia officia deserunt irure. Cupidatat dolor cupidatat ipsum minim consequat Lorem adipisicing. Labore fugiat cupidatat nostrud voluptate ea eu pariatur non. Ipsum quis occaecat irure amet esse eu fugiat deserunt incididunt Lorem esse duis occaecat mollit.",
+		Gender: "male",
+	},
+	{
+		Id:     11,
+		Name:   "Gilmore Guerra",
+		Age:    32,
+		About:  "Labore consectetur do sit et mollit non incididunt. Amet aute voluptate enim et sit Lorem elit. Fugiat proident ullamco ullamco sint pariatur deserunt eu nulla consectetur culpa eiusmod. Veniam irure et deserunt consectetur incididunt ad ipsum sint. Consectetur voluptate adipisicing aute fugiat aliquip culpa qui nisi ut ex esse ex. Sint et anim aliqua pariatur.",
+		Gender: "male",
+	},
+	{
+		Id:     12,
+		Name:   "Cruz Guerrero",
+		Age:    36,
+		About:  "Sunt enim ad fugiat minim id esse proident laborum magna magna. Velit anim aliqua nulla laborum consequat veniam reprehenderit enim fugiat ipsum mollit nisi. Nisi do reprehenderit aute sint sit culpa id Lorem proident id tempor. Irure ut ipsum sit non quis aliqua in voluptate magna. Ipsum non aliquip quis incididunt incididunt aute sint. Minim dolor in mollit aute duis consectetur.",
+		Gender: "male",
+	},
+	{
+		Id:     20,
+		Name:   "Lowery York",
+		Age:    27,
+		About:  "Dolor enim sit id dolore enim sint nostrud deserunt. Occaecat minim enim veniam proident mollit Lorem irure ex. Adipisicing pariatur adipisicing aliqua amet proident velit. Magna commodo culpa sit id.",
+		Gender: "male",
+	},
+	{
+		Id:     23,
+		Name:   "Gates Spencer",
+		Age:    21,
+		About:  "Dolore magna magna commodo irure. Proident culpa nisi veniam excepteur sunt qui et laborum tempor. Qui proident Lorem commodo dolore ipsum.",
+		Gender: "male",
+	},
+	{
+		Id:     34,
+		Name:   "Kane Sharp",
+		Age:    34,
+		About:  "Lorem proident sint minim anim commodo cillum. Eiusmod velit culpa commodo anim consectetur consectetur sint sint labore. Mollit consequat consectetur magna nulla veniam commodo eu ut et. Ut adipisicing qui ex consectetur officia sint ut fugiat ex velit cupidatat fugiat nisi non. Dolor minim mollit aliquip veniam nostrud. Magna eu aliqua Lorem aliquip.",
+		Gender: "male",
+	},
+}
+
+func TestSearchServerHandler(t *testing.T) {
+	testCases := []ServerTestCase{
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}},
+			Result:      testUsers,
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_field": []string{"Id"}, "order_by": []string{"-1"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Id < users[j].Id })
+				return users
+			}(),
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_field": []string{"Id"}, "order_by": []string{"1"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Id > users[j].Id })
+				return users
+			}(),
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_field": []string{"Age"}, "order_by": []string{"-1"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Age < users[j].Age })
+				return users
+			}(),
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_field": []string{"Age"}, "order_by": []string{"1"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Age > users[j].Age })
+				return users
+			}(),
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_field": []string{"Name"}, "order_by": []string{"-1"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Name < users[j].Name })
+				return users
+			}(),
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_field": []string{"Name"}, "order_by": []string{"1"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Name > users[j].Name })
+				return users
+			}(),
+		},
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"lorem"}, "order_by": []string{"-1"}, "offset": []string{"2"}, "limit": []string{"3"}},
+			Result: func() []ServerUser {
+				users := make([]ServerUser, len(testUsers))
+				copy(users, testUsers)
+
+				sort.Slice(users, func(i, j int) bool { return users[i].Name < users[j].Name })
+				return users[2:5]
+			}(),
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"order_field": []string{"unknown123"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"order_by": []string{"hello"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"order_by": []string{"123"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"offset": []string{"abc"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"offset": []string{"-10"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"limit": []string{"hello"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"limit": []string{"-30"}},
+		},
+	}
+
+	for num, testCase := range testCases {
+		testName := fmt.Sprintf("testcase %d", num)
+		t.Run(testName, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+
+			query := testCase.QueryParams.Encode()
+			req := httptest.NewRequest(http.MethodGet, "/search?"+query, nil)
+			req.Header.Set("AccessToken", AccessToken)
+
+			SearchServer(rec, req)
+
+			resp := rec.Result()
+			defer func() { _ = resp.Body.Close() }()
+
+			if resp.StatusCode != testCase.StatusCode {
+				t.Errorf(
+					"wrong status code, got %d, expected %d",
+					resp.StatusCode, testCase.StatusCode,
+				)
+			}
+
+			if testCase.Result == nil {
+				return
+			}
+
+			var users []ServerUser
+			if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
+				t.Errorf("decoding response body unexpected error %v", err)
+			}
+
+			if !reflect.DeepEqual(users, testCase.Result) {
+				t.Errorf("wrong result, got %v, expected %v", users, testCase.Result)
+			}
+		})
+	}
+}
+
+func TestSearchServerInvalidJSONResponse(t *testing.T) {
+	testCases := []ServerTestCase{
+		{
+			StatusCode:  http.StatusOK,
+			QueryParams: url.Values{"query": []string{"__success_json_response"}},
+		},
+		{
+			StatusCode:  http.StatusBadRequest,
+			QueryParams: url.Values{"query": []string{"__bad_json_response"}},
+		},
+		{
+			StatusCode:  http.StatusInternalServerError,
+			QueryParams: url.Values{"query": []string{"__timeout"}},
+		},
+		{
+			StatusCode:  http.StatusInternalServerError,
+			QueryParams: url.Values{"query": []string{"__internal_server_err"}},
+		},
+	}
+
+	for num, testCase := range testCases {
+		testName := fmt.Sprintf("testcase %d", num)
+		t.Run(testName, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+
+			query := testCase.QueryParams.Encode()
+			req := httptest.NewRequest(http.MethodGet, "/search?"+query, nil)
+			req.Header.Set("AccessToken", AccessToken)
+
+			SearchServer(rec, req)
+
+			resp := rec.Result()
+			defer func() { _ = resp.Body.Close() }()
+
+			if resp.StatusCode != testCase.StatusCode {
+				t.Errorf(
+					"wrong status code, got %d, expected %d",
+					resp.StatusCode, testCase.StatusCode,
+				)
+			}
+
+			var users []ServerUser
+			if err := json.NewDecoder(resp.Body).Decode(&users); err == nil {
+				t.Errorf("expecting decoding response body error, got nil")
+			}
+		})
+	}
+}
+
+func TestSearchServerUnauthorized(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/search", nil)
+
+	SearchServer(rec, req)
+
+	resp := rec.Result()
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Errorf("wrong status code, got %d, expected %d", resp.StatusCode, http.StatusUnauthorized)
 	}
 }

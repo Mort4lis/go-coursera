6054f7c675a2de6fb972df7485a58c7f0ebee505
Pavel Korchagin <mortalis94@gmail.com>
2021-09-07 12:15:27 +0300

    HW-7: Implemented graceful shutdown

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -1,7 +1,35 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func asyncServe(ctx context.Context, server *grpc.Server, lis net.Listener) {
+	go func() {
+		if err := server.Serve(lis); err != nil {
+			log.Printf("failed to serve grpc server: %v", err)
+		}
+		fmt.Println("server was successfully shutdown!")
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Println("context fired, start to graceful server shutdown...")
+		server.GracefulStop()
+	}
+}
 
 func StartMyMicroservice(ctx context.Context, addr, acl string) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("failed to listen tcp socket %s: %v", addr, err)
+	}
+
+	server := grpc.NewServer()
+	go asyncServe(ctx, server, lis)
 	return nil
 }

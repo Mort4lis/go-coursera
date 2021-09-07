ce88933fcbd7faae99a1ecb5c40b28655f6eea71
Pavel Korchagin <mortalis94@gmail.com>
2021-09-07 22:20:40 +0300

    HW-7: Implemented stream logging

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -7,40 +7,56 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 type BizManager struct{}
 
-func (b BizManager) Check(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+func (b *BizManager) Check(ctx context.Context, nothing *Nothing) (*Nothing, error) {
 	return &Nothing{Dummy: true}, nil
 }
 
-func (b BizManager) Add(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+func (b *BizManager) Add(ctx context.Context, nothing *Nothing) (*Nothing, error) {
 	return &Nothing{Dummy: true}, nil
 }
 
-func (b BizManager) Test(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+func (b *BizManager) Test(ctx context.Context, nothing *Nothing) (*Nothing, error) {
 	return &Nothing{Dummy: true}, nil
 }
 
-type AdminManager struct{}
-
-func (a AdminManager) Logging(nothing *Nothing, stream Admin_LoggingServer) error {
-	stream.Send(&Event{
-		Timestamp: 0,
-		Consumer:  "",
-		Method:    "",
-		Host:      "",
-	})
-	return nil
+type AdminManager struct {
+	publisher *MethodCallPublisher
 }
 
-func (a AdminManager) Statistics(interval *StatInterval, stream Admin_StatisticsServer) error {
+func (a *AdminManager) Logging(nothing *Nothing, stream Admin_LoggingServer) error {
+	eventCh, subscribeID := a.publisher.Subscribe()
+	defer a.publisher.Unsubscribe(subscribeID)
+
+	for {
+		select {
+		case event := <-eventCh:
+			if err := stream.Send(&Event{
+				Timestamp: event.Timestamp,
+				Consumer:  event.Consumer,
+				Method:    event.Method,
+				Host:      event.Host,
+			}); err != nil {
+				return status.Error(codes.Internal, "failed to send event")
+			}
+		case <-stream.Context().Done():
+			return nil
+		}
+	}
+}
+
+func (a *AdminManager) Statistics(interval *StatInterval, stream Admin_StatisticsServer) error {
 	stream.Send(&Stat{
 		Timestamp:  0,
 		ByMethod:   nil,
@@ -118,6 +134,52 @@ func (i *ACLMethodManager) HasAccess(ctx context.Context, method string) bool {
 	return false
 }
 
+type MethodCallEvent struct {
+	Timestamp int64
+	Consumer  string
+	Method    string
+	Host      string
+}
+
+type MethodCallPublisher struct {
+	mu          sync.RWMutex
+	subscribers map[int]chan MethodCallEvent
+
+	idCounter int64
+}
+
+func NewMethodCallPublisher() *MethodCallPublisher {
+	return &MethodCallPublisher{
+		subscribers: make(map[int]chan MethodCallEvent),
+	}
+}
+
+func (p *MethodCallPublisher) Notify(event MethodCallEvent) {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	event.Timestamp = time.Now().Unix()
+	for _, ch := range p.subscribers {
+		ch <- event
+	}
+}
+
+func (p *MethodCallPublisher) Subscribe() (ch <-chan MethodCallEvent, subscribeID int) {
+	subscriberID := int(atomic.AddInt64(&p.idCounter, 1))
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.subscribers[subscriberID] = make(chan MethodCallEvent)
+
+	return p.subscribers[subscriberID], subscriberID
+}
+
+func (p *MethodCallPublisher) Unsubscribe(subscribeID int) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	delete(p.subscribers, subscribeID)
+}
+
 func aclUnaryInterceptor(manager *ACLMethodManager) grpc.UnaryServerInterceptor {
 	return func(
 		ctx context.Context,
@@ -152,6 +214,72 @@ func aclStreamInterceptor(manager *ACLMethodManager) grpc.StreamServerIntercepto
 	}
 }
 
+func notifyUnaryMethodCallInterceptor(publisher *MethodCallPublisher) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (resp interface{}, err error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return handler(ctx, req)
+		}
+
+		pr, ok := peer.FromContext(ctx)
+		if !ok {
+			return handler(ctx, req)
+		}
+
+		consumers := md.Get("consumer")
+		if len(consumers) == 0 {
+			return handler(ctx, req)
+		}
+
+		event := MethodCallEvent{
+			Consumer: consumers[0],
+			Method:   info.FullMethod,
+			Host:     pr.Addr.String(),
+		}
+		publisher.Notify(event)
+
+		return handler(ctx, req)
+	}
+}
+
+func notifyStreamMethodCallInterceptor(publisher *MethodCallPublisher) grpc.StreamServerInterceptor {
+	return func(
+		srv interface{},
+		stream grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) error {
+		md, ok := metadata.FromIncomingContext(stream.Context())
+		if !ok {
+			return handler(srv, stream)
+		}
+
+		pr, ok := peer.FromContext(stream.Context())
+		if !ok {
+			return handler(srv, stream)
+		}
+
+		consumers := md.Get("consumer")
+		if len(consumers) == 0 {
+			return handler(srv, stream)
+		}
+
+		event := MethodCallEvent{
+			Consumer: consumers[0],
+			Method:   info.FullMethod,
+			Host:     pr.Addr.String(),
+		}
+		publisher.Notify(event)
+
+		return handler(srv, stream)
+	}
+}
+
 func asyncServe(ctx context.Context, server *grpc.Server, lis net.Listener) {
 	go func() {
 		if err := server.Serve(lis); err != nil {
@@ -182,12 +310,20 @@ func StartMyMicroservice(ctx context.Context, addr, aclStr string) error {
 		manager: aclManager,
 	}
 
+	publisher := NewMethodCallPublisher()
+
 	server := grpc.NewServer(
-		grpc.UnaryInterceptor(aclUnaryInterceptor(aclMethodManager)),
-		grpc.StreamInterceptor(aclStreamInterceptor(aclMethodManager)),
+		grpc.ChainUnaryInterceptor(
+			aclUnaryInterceptor(aclMethodManager),
+			notifyUnaryMethodCallInterceptor(publisher),
+		),
+		grpc.ChainStreamInterceptor(
+			aclStreamInterceptor(aclMethodManager),
+			notifyStreamMethodCallInterceptor(publisher),
+		),
 	)
 	RegisterBizServer(server, &BizManager{})
-	RegisterAdminServer(server, &AdminManager{})
+	RegisterAdminServer(server, &AdminManager{publisher: publisher})
 
 	go asyncServe(ctx, server, lis)
 	return nil
